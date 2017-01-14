c4cb5bd7e0c3087d3fd7c83d86eb870ff2b0adf3
Cyph3r - Matt Gakowski <mgakowski@gmail.com>
2017-01-14 17:09:00 +1000

    Update search.py
    added symbol characters to "chars" var.
    Changed "rand_length" var to 8 and added comment for easy changing and reference.
    changed "clear" var to support added desired cleartext string as well as comment for ease of use.

--- search.py
+++ search.py
@@ -1,15 +1,12 @@
+__authors__ = ['Chick3nputer', 'Supersam654', 'MGakowski']
 
-__authors__ = ['Chick3nputer', 'Supersam654']
-
-from itertools import islice, product
-import string
 import hashlib
 import multiprocessing
 from multiprocessing import Process
 from random import shuffle
-from sys import argv
 
-chars = "0123456789abcdefefghijklmnopqrstuvwxyzQAZWSXEDCRFVTGBYHNUJMIKOLP"
+chars = "0123456789abcdefefghijklmnopqrstuvwxyzQAZWSXEDCRFVTGBYHNUJMIKOLP!@#$%^&*()_+-={}[]\|;:',<.>/?`~"  # symbol add
+
 
 def generate_strings(size):
     alphabet = list(chars * size)
@@ -18,29 +15,32 @@ def generate_strings(size):
         for i in range(0, len(alphabet), size):
             yield ''.join(alphabet[i: i + size])
 
+
 def tsum(hexhash):
     b = bytearray.fromhex(hexhash)
     return sum(b)
-    
+
+
 def nibsum(nibhash):
     b = bytearray.fromhex(nibhash)
     nsum = 0
     for byte in b:
-        nsum += byte/16
+        nsum += byte / 16
         nsum += byte % 16
     return nsum
-    
+
+
 def work():
     # Start both not at 0 and 160 to avoid a lot of startup noise.
-    max_ones = 126
-    min_ones = 33
-    rand_length = 12
+    max_ones = 128
+    min_ones = 32
+    rand_length = 8 # Modify for desired rand sring length
     i = 0
     for combo in generate_strings(rand_length):
         i += 1
         if i % 100000000 == 0:
             print "Processed %d hashes." % i
-        clear = combo
+        clear = combo+""  # Modify/Add something between quotes for desired entry in clear text.
         hashhex = hashlib.sha1(clear).hexdigest()
 
         ones_count = bin(int(hashhex, 16))[2:].count('1')
@@ -59,20 +59,23 @@ def work():
             print "New MIN Hash Found %s:%s" % (hashhex, clear)
 
         tsumhex = tsum(hashhex)
-        if tsumhex < 481:
-            print "New Byte MIN Hash Found %s:%s" % (hashhex, clear)
-        elif tsumhex > 4639:
-            print "New Byte MAX Hash Found %s:%s" % (hashhex, clear)
-        
+        if tsumhex < 423:
+            print "New Byte MIN Hash Found %s:%s:%s" % (hashhex, clear, tsumhex)
+        elif tsumhex > 4728:
+            print "New Byte MAX Hash Found %s:%s:%s" % (hashhex, clear, tsumhex)
+
         nibsumhex = nibsum(hashhex)
-        if nibsumhex < 106:
-            print "New Nib MIN Hash Found %s:%s" % (hashhex, clear)
-        elif nibsumhex > 494:
-            print "New Nib MAX Hash Found %s:%s" % (hashhex, clear)
+        if nibsumhex < 90:
+            print "New Nib MIN Hash Found %s:%s:%s" % (hashhex, clear, nibsumhex)
+        elif nibsumhex > 509:
+            print "New Nib MAX Hash Found %s:%s:%s" % (hashhex, clear, nibsumhex)
+
 
 if __name__ == '__main__':
     count = multiprocessing.cpu_count()
     for i in range(0, (count - 1)):
         p = Process(target=work)
         p.start()
-        print "Starting worker %s" % (i+1)
+        print "Starting worker %s" % (i + 1)
+
+work()
